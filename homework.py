3c4b863f5bbe005912a62f14a3c22711f0abd54e
Alexey Erёmenko <n8240782@yandex.ru>
2021-11-03 18:21:43 +0300

    edit homework.py and change other files

--- homework.py
+++ homework.py
@@ -1,68 +1,94 @@
-class InfoMessage:
-    """Информационное сообщение о тренировке."""
-    pass
+
+import datetime as dt
+
+DATE_PATTERN = '%d.%m.%Y'
 
 
-class Training:
-    """Базовый класс тренировки."""
+class Calculator:
+    """Материнский класс калькулятора денег и каллорий."""
+    def __init__(self, limit):
+        self.limit = limit
+        self.records = []
 
-    def __init__(self,
-                 action: int,
-                 duration: float,
-                 weight: float,
-                 ) -> None:
-        pass
+    def add_record(self, record):
+        """Транслируем запрос во внешний класс для обработки данных."""
+        self.records.append(record)
 
-    def get_distance(self) -> float:
-        """Получить дистанцию в км."""
-        pass
+    def get_today_stats(self):
+        """Подсчёт данных за день."""
+        now_date = dt.date.today()
+        return sum(days.amount for days in self.records
+                   if days.date == now_date)
 
-    def get_mean_speed(self) -> float:
-        """Получить среднюю скорость движения."""
-        pass
+    def get_week_stats(self):
+        """Подсчёт данных за неделю."""
+        now_date = dt.date.today()
+        old_date = now_date - dt.timedelta(days=7)
+        return sum(days.amount for days in self.records
+                   if old_date < days.date <= now_date)
 
-    def get_spent_calories(self) -> float:
-        """Получить количество затраченных калорий."""
-        pass
-
-    def show_training_info(self) -> InfoMessage:
-        """Вернуть информационное сообщение о выполненной тренировке."""
-        pass
-
-
-class Running(Training):
-    """Тренировка: бег."""
-    pass
+    def get_difference(self):
+        """Вычисление разницы между заданным лимитом и состоянием на
+        текущий момент.
+        """
+        return self.limit - self.get_today_stats()
 
 
-class SportsWalking(Training):
-    """Тренировка: спортивная ходьба."""
-    pass
+class CaloriesCalculator(Calculator):
+    """Класс калькулятора каллорий."""
+    REMAINED_CALORIES = ('Сегодня можно съесть что-нибудь ещё, но с общей '
+                         'калорийностью не более {calories} кКал')
+    LIMIT = 'Хватит есть!'
+
+    def get_calories_remained(self):
+        """Остаток каллорий"""
+        calories = self.get_difference()
+        if calories <= 0:
+            return 'Хватит есть!'
+        return ('Сегодня можно съесть что-нибудь ещё, но с общей '
+                f'калорийностью не более {calories} кКал')
 
 
-class Swimming(Training):
-    """Тренировка: плавание."""
-    pass
+class CashCalculator(Calculator):
+    """Класс калькулятора расходов."""
+    USD_RATE = 72.88
+    EURO_RATE = 85.47
+    RUB_RATE = 1
+
+    CURRENCIES = {
+        'rub': ('руб', RUB_RATE),
+        'usd': ('USD', USD_RATE),
+        'eur': ('Euro', EURO_RATE),
+    }
+
+    def get_today_cash_remained(self, currency: str):
+        """Функция отчёта о состоянии дневного баланса с конвертацией в
+        необходимую валюту.
+        """
+        if currency not in self.CURRENCIES:
+            raise ValueError(f'Ошибка: Валюта "{currency}" не '
+                             'поддерживается')
+        cash = self.get_difference()
+        if cash == 0:
+            return 'Денег нет, держись'
+        cur_name, cur_money = self.CURRENCIES[currency]
+        cash = round(cash / cur_money, 2)
+        if cash > 0:
+            return (f'На сегодня осталось {cash} {cur_name}')
+        cash = abs(cash)
+        return (f'Денег нет, держись: твой долг - {cash} {cur_name}')
 
 
-def read_package(workout_type: str, data: list) -> Training:
-    """Прочитать данные полученные от датчиков."""
-    pass
-
-
-def main(training: Training) -> None:
-    """Главная функция."""
-    pass
+class Record:
+    """Обработка данных, возможно встраивание проверок на соответствие"""
+    def __init__(self, amount, comment, date=None):
+        self.amount = abs(amount)
+        self.comment = comment
+        if date is None:
+            self.date = dt.date.today()
+        else:
+            self.date = dt.datetime.strptime(date, DATE_PATTERN).date()
 
 
 if __name__ == '__main__':
-    packages = [
-        ('SWM', [720, 1, 80, 25, 40]),
-        ('RUN', [15000, 1, 75]),
-        ('WLK', [9000, 1, 75, 180]),
-    ]
-
-    for workout_type, data in packages:
-        training = read_package(workout_type, data)
-        main(training)
-
+    pass
